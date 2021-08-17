c82ad6ab1b552f3562f92bf3bb6991b7b2d557b0
James Hibbeard <jhibbeard@gmail.com>
2021-08-17 17:10:53 +0200

    Building of Seed file and addition of images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+
+User.create([
+  email: 's.hawkins@googlemail.com',
+  encrypted_password: "Password1234",
+  user_name: 'shawks',
+  first_name: 'Sam',
+  last_name: 'Hawkins',
+  address: '35 Langview Drive, San Francisco, USA, 134245',
+  phone_number: '+1 417 234 1123',
+])
+
+
+
+
+meals = ['Italien', 'Greek', 'French', 'American', 'British']
+
+file = URI.open()
+meal = Meal.create([
+  meal_name: '',
+  category: meals[ran_number],
+  description: ,
+  user_id:
+  ])
+  meal.photo.attach(file)
+
+file = URI.open()
+meal = Meal.create([
+  meal_name: ,
+  category: meals[ran_number],
+  description: ,
+  user_id:
+  ])
+  meal.photo.attach(file)
