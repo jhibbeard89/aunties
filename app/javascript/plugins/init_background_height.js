3643d45bd24b57d168639dfd6608eb0a2912b2ae
James Hibbeard <jhibbeard@gmail.com>
2021-08-20 00:49:51 +0200

    small bug fix with new JS code

--- app/javascript/plugins/init_background_height.js
+++ app/javascript/plugins/init_background_height.js
@@ -1,6 +1,9 @@
 const background_page_height = () => {
   let page_height = document.body.scrollHeight
-  document.getElementById('background-image').style.height = `${page_height}px`;
+  const backgroundImage = document.getElementById('background-image')
+  if (backgroundImage){
+    backgroundImage.style.height = `${page_height}px`;
+  }
 }
 
 export {background_page_height}
