0164b35fc9892343f3840335b700638400877013
James Hibbeard <jhibbeard@gmail.com>
2021-08-19 19:49:32 +0200

    Bug fixed for Branch and Seed file updated with real addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 puts "hello"
-puts "Lets clear your db"
-Meal.delete_all
-puts "Meals are deleted"
-User.delete_all
-puts "Users are deleted"
 
 puts "Let's seed"
 # User Seeds
@@ -21,7 +16,7 @@ user = User.new(
   password: "Password1234",
   first_name: 'Sam',
   last_name: 'Hawkins',
-  address: '35 Langview Drive, San Francisco, USA, 134245',
+  address: '71 Schillerstr, Berlin, 10627 ',
   phone_number: '+1 417 234 1123',
   auntie: false
 )
@@ -34,7 +29,7 @@ user = User.new(
   password: "Password1234",
   first_name: 'Andrew',
   last_name: 'Cox',
-  address: '12 Cook Av, San Francisco, USA, 134245',
+  address: '13 Grolmanstr, Berlin, 10623',
   phone_number: '+1 417 234 1123',
   auntie: true
 )
@@ -47,7 +42,7 @@ user = User.new(
   password: "Password1234",
   first_name: 'Pauline',
   last_name: 'Lane',
-  address: 'The Range, Piearson Grove, San Francisco, USA, 134245',
+  address: '13 Wilsnackerstr, Berlin, 10559',
   phone_number: '+1 417 234 1123',
   auntie: true
 )
@@ -61,49 +56,50 @@ user = User.new(
   password: "Password1234",
   first_name: 'India',
   last_name: 'Smith',
-  address: 'Palm Road, San Francisco, USA, 134245',
+  address: 'Gerhardtstraße 1, Berlin, 10557',
   phone_number: '+1 417 234 1123',
   auntie: false
 )
 user.photo.attach(io: file, filename: 'ISmith.jpg', content_type: 'image/jpg')
 user.save
 
-
 file = URI.open('https://img.taste.com.au/PXvnKf8Y/taste/2016/11/spaghetti-bolognaise-104579-1.jpeg')
 meal = Meal.new(
   meal_name: 'Homly Spaghetti',
   category: "Italien",
   description: "A lovely and homely spaghetti Bolognais",
+  address: 'Habersaathstraße 13, 10115 Berlin',
   price: rand(4..12),
   quantity: rand(2..10),
   user: User.find(2)
-  )
-  meal.photo.attach(io: file, filename: 'spagbol.jpeg', content_type: 'image/jpeg')
-  meal.save
+)
+meal.photo.attach(io: file, filename: 'spagbol.jpeg', content_type: 'image/jpeg')
+meal.save
 
-  file = URI.open('https://food-images.files.bbci.co.uk/food/recipes/chicken_pie_24044_16x9.jpg')
+file = URI.open('https://food-images.files.bbci.co.uk/food/recipes/chicken_pie_24044_16x9.jpg')
 meal = Meal.new(
   meal_name: 'Chicken pie',
-  category: "British",
-  description: "A traditional english chicken pie",
+  category: 'British',
+  description: 'A traditional english chicken pie',
+  address: 'Habersaathstrasse 13, 10115 Berlin',
   price: rand(4..12),
   quantity: rand(2..10),
   user: User.find(3)
-  )
-  meal.photo.attach(io: file, filename: 'chickenPie.jpg', content_type: 'image/jpg')
-  meal.save
+)
+meal.photo.attach(io: file, filename: 'chickenPie.jpg', content_type: 'image/jpg')
+meal.save!
 
-
-  file = URI.open('https://static01.nyt.com/images/2021/01/15/dining/15COOKING-OLD-BEEF-STEW2/15COOKING-OLD-BEEF-STEW2-articleLarge-v2.jpg')
+file = URI.open('https://static01.nyt.com/images/2021/01/15/dining/15COOKING-OLD-BEEF-STEW2/15COOKING-OLD-BEEF-STEW2-articleLarge-v2.jpg')
 meal = Meal.new(
   meal_name: 'West Coast Beef Stew',
   category: "American",
-  description: "A fine example of west coast cuisine in this west coast beef stew.",
+  description: 'A fine example of west coast cuisine in this west coast beef stew.',
+  address: 'Tuchollapl. 2, 10317 Berlin',
   price: rand(4..12),
   quantity: rand(2..10),
   user: User.find(2)
-  )
-  meal.photo.attach(io: file, filename: 'beefstew.jpg', content_type: 'image/jpg')
-  meal.save
+)
+meal.photo.attach(io: file, filename: 'beefstew.jpg', content_type: 'image/jpg')
+meal.save
 
 puts "Goodbye"
